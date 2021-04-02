859627c25cf59f7dc487c68927d5b9d2972a8fa3
0elle0 <el_leo_m@hotmail.com>
2021-04-02 19:26:10 +0200

    first commit

---
+++ src/main/java/dk/kea/stud/dls/schoolprotocol/SchoolProtocolApplication.java
@@ -0,0 +1,13 @@
+package dk.kea.stud.dls.schoolprotocol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolProtocolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolProtocolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dk/kea/stud/dls/schoolprotocol/SchoolProtocolApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.kea.stud.dls.schoolprotocol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolProtocolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
