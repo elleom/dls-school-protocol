2c6c5db2f6314a03ff9a4b8316b44e9d28e6eb8f
0elle0 <el_leo_m@hotmail.com>
2021-04-05 17:50:23 +0200

    optimize code

--- src/main/java/dk/kea/stud/dls/schoolprotocol/controller/Main.java
+++ src/main/java/dk/kea/stud/dls/schoolprotocol/controller/Main.java
@@ -1,10 +1,7 @@
 package dk.kea.stud.dls.schoolprotocol.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class Main {
