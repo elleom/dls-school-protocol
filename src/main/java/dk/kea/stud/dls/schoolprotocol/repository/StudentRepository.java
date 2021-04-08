0e5c3bc91341022ea6d9c9ef6f60bb4e0c384448
0elle0 <el_leo_m@hotmail.com>
2021-04-08 11:53:18 +0200

    refactoring & clean up

--- src/main/java/dk/kea/stud/dls/schoolprotocol/repository/StudentRepository.java
+++ src/main/java/dk/kea/stud/dls/schoolprotocol/repository/StudentRepository.java
@@ -4,4 +4,5 @@ import dk.kea.stud.dls.schoolprotocol.model.Student;
 import org.springframework.data.repository.CrudRepository;
 
 public interface StudentRepository extends CrudRepository<Student, Long> {
+
 }
