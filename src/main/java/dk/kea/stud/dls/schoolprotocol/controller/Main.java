f211208a8a5c900739abc8043835c090632326f8
0elle0 <el_leo_m@hotmail.com>
2021-04-10 00:19:20 +0200

    \teacher => dynamic id on main

--- src/main/java/dk/kea/stud/dls/schoolprotocol/controller/Main.java
+++ src/main/java/dk/kea/stud/dls/schoolprotocol/controller/Main.java
@@ -1,8 +1,11 @@
 package dk.kea.stud.dls.schoolprotocol.controller;
 
 import dk.kea.stud.dls.schoolprotocol.model.Subject;
+import dk.kea.stud.dls.schoolprotocol.model.Teacher;
 import dk.kea.stud.dls.schoolprotocol.repository.SubjectRepository;
+import dk.kea.stud.dls.schoolprotocol.repository.TeacherRepository;
 import dk.kea.stud.dls.schoolprotocol.service.RequestService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,24 +15,30 @@ import javax.servlet.http.HttpServletRequest;
 @Controller
 public class Main {
 
+    @Autowired
+    RequestService requestService;
+    @Autowired
+    TeacherRepository teacherRepository;
 
-    private RequestService requestService;
 
-    public Main(RequestService requestService, SubjectRepository subjectRepository) {
-        this.requestService = requestService;
-
-    }
 
     @RequestMapping({"", "index", "index.html", "/"})
     public String getIndex(HttpServletRequest request, Model model){ //add model to load repos
+
+        String userName = request.getRemoteUser();
+        Teacher teacher = teacherRepository.findById(1L).get();
 
 
         String clientIp = requestService.getClientIp(request);
         String clientMAC = requestService.getClientMac(clientIp);
         model.addAttribute("clientIp", clientIp);
         model.addAttribute("clientMAC", clientMAC);
+        model.addAttribute("teacher", teacher);
+
         return "index";
     }
+
+
 
 
 }
--- src/main/java/dk/kea/stud/dls/schoolprotocol/controller/TeacherController.java
+++ src/main/java/dk/kea/stud/dls/schoolprotocol/controller/TeacherController.java
@@ -3,24 +3,25 @@ package dk.kea.stud.dls.schoolprotocol.controller;
 import dk.kea.stud.dls.schoolprotocol.model.Subject;
 import dk.kea.stud.dls.schoolprotocol.repository.SubjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
 
+@Controller
 public class TeacherController {
 
     @Autowired
     SubjectRepository subjectRepository;
 
-    @RequestMapping({"/teacher/", "/teacher/dashboard", "/teacher/dashboard.html"})
-    public String getDashBoard(HttpServletRequest request, Model model){ //add model to load repos
-
+    @RequestMapping({"/teacher/dashboard", "/teacher/dashboard.html"})
+    public String getDashBoard(@Param("id") Long id, HttpServletRequest request, Model model){ //add model to load repos
         //tryout OK
-        Iterable<Subject> subjects = subjectRepository.findAllByTeacher(1L);
-
+        Iterable<Subject> subjects = subjectRepository.findAllByTeacher(id);
         model.addAttribute("subjects", subjects);
-        return "index";
+        return "teacher_dashboard";
     }
 
 }
