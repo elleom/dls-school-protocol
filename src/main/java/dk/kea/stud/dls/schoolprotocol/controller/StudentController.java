6d179a913070c17393913458b3d2b3a85f7bf744
0elle0 <el_leo_m@hotmail.com>
2021-04-10 14:34:36 +0200

    student object

---
+++ src/main/java/dk/kea/stud/dls/schoolprotocol/controller/StudentController.java
@@ -0,0 +1,27 @@
+package dk.kea.stud.dls.schoolprotocol.controller;
+
+import dk.kea.stud.dls.schoolprotocol.model.Subject;
+import dk.kea.stud.dls.schoolprotocol.repository.SubjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequestMapping("/api")
+public class StudentController {
+
+    @Autowired
+    SubjectRepository subjectRepository;
+
+    @RequestMapping({"/student/dashboard", "/student/dashboard.html"})
+    public String getDashBoard(@Param("id") Long id, HttpServletRequest request, Model model){ //add model to load repos
+        //tryout OK
+        Iterable<Subject> subjects = subjectRepository.findAllByTeacher(id);
+        model.addAttribute("subjects", subjects);
+        return "student_dashboard";
+    }
+}
