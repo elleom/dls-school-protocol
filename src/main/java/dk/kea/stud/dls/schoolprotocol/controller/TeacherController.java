5bfb021313718c0f68cb46f5b05a861926ad3c8c
oska3728 <oska3728@stud.kea.dk>
2021-05-12 23:59:41 +0200

    code generator and layout update

--- src/main/java/dk/kea/stud/dls/schoolprotocol/controller/TeacherController.java
+++ src/main/java/dk/kea/stud/dls/schoolprotocol/controller/TeacherController.java
@@ -13,11 +13,12 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
 import javax.servlet.http.HttpServletRequest;
-import javax.transaction.Transactional;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
+import java.util.Random;
+
+
 
 @Controller
 @RequestMapping("/api")
@@ -40,6 +41,7 @@ public class TeacherController {
         //tryout OK
         String userName = request.getRemoteUser();
         Teacher teacher = teacherRepository.findByUserName(userName);
+        model.addAttribute("user", teacher);
         Iterable<Subject> subjects = subjectRepository.findAllByTeacher(id);
         model.addAttribute("subjects", subjects);
         return "teacher_dashboard";
@@ -59,20 +61,42 @@ public class TeacherController {
     }
 
     @PostMapping("/teacher/subjectDetails")
-    public String registerNewLesson (@Param("subjectId") Long subjectId){
-//        LessonRepository.addNewLesson(lesson);
-        //recover subject
-        Subject subject = subjectRepository.findById(subjectId).get();
-        Lesson lessons = new Lesson();
-        lessons.setCode("TRY7");
-        lessons.setSubject(subject);
-        Timestamp timeCreated = new Timestamp(32132132132L);
-        lessons.setDate(timeCreated);
+    public String registerNewLesson (@Param("subjectId") Long subjectId,@Param("id") Long id, HttpServletRequest request, Model model){
 
-        //try
-         //if OK
+        //Class code generation
+        String lessonCode = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890";
+        StringBuilder salt = new StringBuilder();
+        Random random = new Random();
+        while (salt.length() < 6) { // length of the random string.
+            int index = (int) (random.nextFloat() * lessonCode.length());
+            salt.append(lessonCode.charAt(index));
+        }
+        //factoring to string
+        String saltStr = salt.toString();
+
+        String userName = request.getRemoteUser();
+        Teacher teacher = teacherRepository.findByUserName(userName);
+        model.addAttribute("user", teacher);
+
+        Iterable<Subject> subjects = subjectRepository.findAllByTeacher(id);
+        model.addAttribute("subjects", subjects);
+        Subject subject = subjectRepository.findById(subjectId).get();
+
+        Lesson lessons = new Lesson();
+        lessons.setCode(saltStr);
+        lessons.setSubject(subject);
+
+        LocalDateTime now = LocalDateTime.now();
+        Timestamp timeCreated = Timestamp.valueOf(now);
+
+        lessons.setDate(timeCreated);
         lessonRepository.save(lessons);
-            //return OK template
+//        if (lessonRepository.count() > oldCountOfClasses){
+//            return "codeGenerationSuccess";
+//        }
+//        else {
+//            return "teacher_dashboard";
+//        }    //return OK template
         //catch
             //return Error template
         return "codeGenerationSuccess";
--- src/main/java/dk/kea/stud/dls/schoolprotocol/repository/LessonRepository.java
+++ src/main/java/dk/kea/stud/dls/schoolprotocol/repository/LessonRepository.java
@@ -17,13 +17,4 @@ public interface LessonRepository extends CrudRepository<Lesson, Long> {
 
     @Query(value = "select * from lesson where subject_id = :id", nativeQuery = true)
     Iterable<Lesson> getAllbySubject(@Param("id")Long subject_id);
-//    int i = 5;
-//    LocalDateTime myDate = LocalDateTime.now();
-//    String code = "qwe123";
-//    @Query(value = "insert into dls_school_protocol.lesson values (i++, myDate, code, :id)", nativeQuery = true)
-//    Iterable<Lesson> createNewLesson(@Param(""))
-/*
-    @Query(value = "select * from lesson join attendance a on lesson.id = a.lesson_id where a.student_id = :id;", nativeQuery = true)
-    Iterable<Lesson> getAllByStudentAttendance(@Param("id") Long student_id);
-*/
 }
