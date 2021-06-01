8ec4d86582859107dc7308c9ff4e00483458aabe
elleom <el_leo_m@hotmail.com>
2021-06-01 13:54:28 +0200

    added data replication controllers

---
+++ src/main/java/dk/kea/stud/dls/schoolprotocol/persistance/aop/ReadOnlyRouteInterceptor.java
@@ -0,0 +1,34 @@
+package dk.kea.stud.dls.schoolprotocol.persistance.aop;
+
+
+import dk.kea.stud.dls.schoolprotocol.persistance.routing.RoutingDataSource;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Aspect
+@Component
+@Order(0)
+public class ReadOnlyRouteInterceptor {
+
+    private static final Logger logger = LoggerFactory.getLogger(ReadOnlyRouteInterceptor.class);
+
+    @Around("@annotation(transactional)")
+    public Object proceed(ProceedingJoinPoint proceedingJoinPoint, Transactional transactional) throws Throwable {
+        try {
+            if (transactional.readOnly()) {
+                RoutingDataSource.setReplicaRoute();
+                logger.info("Routing database call to the read replica");
+            }
+            return proceedingJoinPoint.proceed();
+        } finally {
+            RoutingDataSource.clearReplicaRoute();
+        }
+    }
+}
